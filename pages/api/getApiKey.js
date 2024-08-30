f2889fb4556702c9e14c3e2f557b27f05cfeb262
zhutyler21 <zhutyler21@gmail.com>
2024-08-30 15:31:26 +0800

    vercel

--- pages/api/getApiKey.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    const apiKey = process.env.API_KEY;
-    if (!apiKey) {
-        return res.status(500).json({ error: 'API key not found' });
-    }
-    res.status(200).json({ apiKey });
-}
