a12e025aeb7ffa0dcd6fe3b39bcc6bddcc5cc942
zhutyler21 <zhutyler21@gmail.com>
2024-08-30 15:15:30 +0800

    api

--- pages/api/getApiKey.js
+++ pages/api/getApiKey.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-    res.status(200).json({ apiKey: process.env.API_KEY });
+    const apiKey = process.env.API_KEY;
+    if (!apiKey) {
+        return res.status(500).json({ error: 'API key not found' });
+    }
+    res.status(200).json({ apiKey });
 }
